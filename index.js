706ed4b2954e990dcd832611be4b46417dccad8a
maribels_are_ringing <maribel.duron86@gmail.com>
2021-04-11 14:41:43 -0700

    all working .js code

--- index.js
+++ index.js
@@ -1,32 +1,111 @@
 const inquirer = require('inquirer');
-const HTML = require('./dist/team.html');
 const fs = require('fs');
-const src = require('src');
-const writeFileAsync = src.promisify(fs.writeFile);
-const Engineer = require('../lib/Engineer');
-const Intern = require('../lib/Intern');
+// const src = require('src');
+// const writeFileAsync = src.promisify(fs.writeFile);
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
 
-class Team {
-    constructor() {
-      this.jobTitle = [];
-      this.manager;
-    }
-
-    createEmployee() {
-        this.jobTitle[Engineer, Intern];
+function init() {
   
-      inquirer
-        .prompt([
-            {
-          type: 'text',
-          name: 'name',
-          message: `Enter ${jobTitle}'s name:`
-            },
-        ]).then(data => writeFileAsync('team.html',html(data)));
+  inquirer.prompt([
+    {
+    type: `input`,
+    name: `mgrName`,
+    message: `Input manager's name:`
+    },
+    {
+    type: `number`,
+    name: `employeeId`,
+    message: `Input manager's ID:`
+    },
+    {
+    type: `input`,
+    name: `emailAddy`,
+    message: `Input manager's email address:`
+    },
+    {
+    type: `input`,
+    name: `officeNum`,
+    message: `Input manager's office number:`
+    }
+    ]).then(answers => {
+      var manager = new Manager(answers.mgrName, answers.employeeId, answers.emailAddy, answers.officeNum)
+        console.log(manager);
+        createEmployee()
+        // writeFileAsync('team.html',html(manager))}
+    });
         };
 
-    };
+        function createEmployee(){
+          inquirer.prompt(
+          {
+          type: 'list',
+          name: 'employeeType',
+          message: 'Would you like to add an Engineer, Intern, or finish building your team?',
+          choices: ['Engineer', 'Intern', 'Finish']
+          },
+          ).then(data => {
 
-createEmployee();
+            if (data.employeeType == 'Engineer'){
+              return inquirer.prompt([
+              {
+              type: 'input',
+              name: 'engName',
+              message: "Input engineer's name:"
+              },
+              {
+              type: 'input',
+              name: 'employeeId',
+              message: "Input engineer's ID:"
+              },
+              {
+              type: 'input',
+              name: 'emailAddy',
+              message: "Input Engineer's email address:"
+              },
+              {
+              type: 'input',
+              name: 'github',
+              message: "Input Engineer's GitHub:"
+              },
+              ]).then(data => {
+                var engineer = new Engineer(data.engName, data.employeeId, data.emailAddy, data.github)
+                console.log(engineer)
+          // writeFileAsync('team.html',html(engineer));
+              })
+            };
+            if (data.employeeType == 'Intern'){
+              return inquirer.prompt([
+              {
+              type: 'input',
+              name: 'internName',
+              message: "Input Intern's name:"
+              },
+              {
+              type: 'input',
+              name: 'employeeId',
+              message: "Input Intern's ID:"
+              },
+              {
+              type: 'input',
+              name: 'emailAddy',
+              message: "Input Interns's email address:"
+              },
+              {
+              type: 'input',
+              name: 'school',
+              message: "Input Intern's school name:"
+              },
+              ]).then(data => {
+                var intern = new Intern(data.internName, data.employeeId, data.emailAddy, data.school)
+                console.log(intern)
+              });
+            };
+            if (data.employeeType == 'Finish'){
+              // writeFileAsync('team.html',html(engineer));
+            }
+          })
 
-  module.exports = Team;
+        };
+init()
