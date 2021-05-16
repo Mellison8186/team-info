bc57dabaad99d703832cdd70aa469920274f1f9c
maribels_are_ringing <maribel.duron86@gmail.com>
2021-05-15 22:20:25 -0700

    updated  js, html, and readme files

--- README.md
+++ README.md
@@ -3,11 +3,15 @@
 ## Purpose
 An app that allows companies to create employees via the command prompt, which then outputs an HTML file of the team in their respective cards.
 
+## Screenshot of Generated HTML Output
+<img width="1437" alt="Screen Shot 2021-05-15 at 10 18 39 PM" src="https://user-images.githubusercontent.com/77599753/118386330-905dff80-b5cb-11eb-8c8c-ed3e0a065e4b.png">
+
 ## Video Link
-https://drive.google.com/file/d/1JZgmioE66NNfkr7i92rxT60F6eAWmXpL/view
+https://drive.google.com/file/d/1oPMg8c2Kb1jKEJTMT4q6LZR8x74cJscV/view
 
 ## Built With
 * HTML
+* CSS
 * JavaScript
 * Bootstrap
 * Jest
--- src/generateHTML.js
+++ src/generateHTML.js
@@ -8,13 +8,18 @@ return`
           <div class="col-4"></div>
             <div class="col-4">
               <div class="card-body">
-                <h5 class="card-title">Manager</h5>
-                <h6 class="card-text">${mgr.name}</h6>
+                <h5 class="card-title" placeholder="Role">${mgr.getRole()}</h5>
+                <label class="h6 card-text">Name: ${mgr.name}</label>
               </div>
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">${mgr.id}</li>
-                <li class="list-group-item"><a href="mailto://" class="card-link">${mgr.email}</a></li>
-                <li class="list-group-item"><a href="#" class="card-link">${mgr.officeNumber}</a></li>
+              <ul style="list-style: none" class="list-group list-group-flush">
+                <li>
+                <label class="list-group-item">ID Number: ${mgr.id}
+                </label>
+                </li>
+                <li>
+                <label class="list-group-item">Email Address: <a href="mailto://" class="card-link">${mgr.email}</a></labe></li>
+                <li>
+                <label class="list-group-item" class="card-link">Office Number: ${mgr.officeNumber}</label></li>
               </ul>
             </div>
           </div>
@@ -34,12 +39,21 @@ const createEng = eng => {
             <div class="col-4">
               <div class="card-body">
                 <h5 class="card-title">${eng.getRole()}</h5>
-                    <h6 class="card-text">${eng.Name}</h6>
+                    <label class="h6 card-text">Name: ${eng.name}</label>
                   </div>
-                  <ul class="list-group list-group-flush">
-                    <li class="list-group-item">${eng.id}</li>
-                    <li class="list-group-item"><a href="mailto://" class="card-link">${eng.email}</a></li>
-                    <li class="list-group-item"><a href="url" target="_blank" class="card-link">${eng.gitHub}</a></li>
+                  <ul style="list-style: none" class="list-group list-group-flush">
+                    <li>
+                    <label class="list-group-item">ID Number: ${eng.id}
+                    </label>
+                    </li>
+                    <li>
+                    <label class="list-group-item">Email Address: <a href="mailto://" class="card-link">${eng.email}</a>
+                    </label>
+                    </li>
+                    <li>
+                    <label class="list-group-item">GitHub: <a href="https://github.com/" target="_blank" class="card-link">${eng.gitHub}</a>
+                    </label>
+                    </li>
                   </ul>
           </div>
         </div>
@@ -57,13 +71,22 @@ const createIntern = empIntern => {
         <div class="col-4"></div>
           <div class="col-4">
             <div class="card-body">
-              <h5 class="card-title">${empIntern.getRole()}</h5>
-                <h6 class="card-text">${empIntern.name}</h6>
+              <h5 class="card-title" placeholder="Role">${empIntern.getRole()}</h5>
+                <label class="h6 card-text">Name: ${empIntern.name}</label>
                   </div>
-                    <ul class="list-group list-group-flush">
-                      <li class="list-group-item">${empIntern.id}</li>
-                      <li class="list-group-item"><a href="mailto://" class="card-link">${empIntern.email}</a></li>
-                      <li class="list-group-item">${empIntern.school}</li>
+                    <ul style="list-style: none" class="list-group list-group-flush">
+                      <li>
+                      <label class="list-group-item">ID Number: ${empIntern.id}
+                      </label>
+                      </li>
+                      <li>
+                      <label class="list-group-item">Email Address: <a href="mailto://" class="card-link">${empIntern.email}</a>
+                      </label>
+                      </li>
+                      <li>
+                      <label class="list-group-item">School Name: ${empIntern.school}
+                      </label>
+                      </li>
                       </ul>
           </div>
         </div>
@@ -71,33 +94,40 @@ const createIntern = empIntern => {
   };
 
 module.exports = generateHTML => {
-// destructure page data  
+// destructure page data
 const { manager, eng, intern, ...header } = generateHTML;
-
-  return `
-  <!DOCTYPE html>
+var htmlString = ''
+for(let i = 0; i<generateHTML.length; i++) {
+  if (generateHTML[i].getRole() === 'Manager') {
+    htmlString += createMgr(generateHTML[i])
+  }
+  if (generateHTML[i].getRole() === 'Engineer') {
+    htmlString += createEng(generateHTML[i])
+  }
+  if (generateHTML[i].getRole() === 'Intern') {
+    htmlString += createIntern(generateHTML[i])
+  }
+}
+  return `<!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-<link rel="stylesheet" href="style.css">
-    <title>Team Info</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+  <link rel="stylesheet" href="./src/style.css">
+  <title>Team Info</title>
 </head>
 
 <body>
   <header class=".col-12">
-    <h1>${header.name}</h1>
+    <h1>My Team</h1>
   </header>
 
     <main class="container">
-    ${createMgr(manager)}
-    ${createEng(eng)}
-    ${createIntern(intern)}
+    ${htmlString}
     </main>
 </body>
-</html>
-  `;
+</html>`;
 };
